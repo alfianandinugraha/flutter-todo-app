4d6a951c4f5734585e31ffc0feb6e080643ad17f
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-29 21:34:41 +0700

    feat(layout): add form and button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -16,8 +18,37 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hello world !"),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
+        child: Column(
+          children: [
+            Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: TextField(),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: MaterialButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Add todo",
+                          style: TextStyle(
+                            color: Colors.white
+                          ),
+                        ),
+                        color: Color.fromRGBO(112, 11, 151, 1),
+                      )
+                    )
+                  ]
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
